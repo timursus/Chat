36da6e3fbb65d9370ca85621b62b884079011720
Timursus <redundant09@gmail.com>
2020-08-11 16:50:50 +0300

    add channel switching

--- src/features/channels/ChannelsList.jsx
+++ src/features/channels/ChannelsList.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import cn from 'classnames';
+import { setCurrentChannel } from './channelsSlice.js';
 
 const ChannelsList = ({ currentChannelId }) => {
   const { channels } = useSelector(
     (state) => state.channelsInfo,
   );
+  const dispatch = useDispatch();
+
+  const goToChannel = (id) => (e) => {
+    e.preventDefault();
+    dispatch(setCurrentChannel({ id }));
+  };
+
   return (
     <nav className="nav nav-pills nav-fill flex-column">
       {channels.map(({ id, name }) => {
         const classes = cn({ 'nav-item nav-link': true, active: currentChannelId === id });
-        return <a className={classes} key={id} href={`#${id}`}>{name}</a>;
+        return (
+          <a className={classes} key={id} href={`#${id}`} onClick={goToChannel(id)}>{name}</a>
+        );
       })}
     </nav>
   );
