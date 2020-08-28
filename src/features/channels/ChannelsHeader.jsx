056cae85ff9fddeadb7df4bc204095a8f4ada053
Timursus <redundant09@gmail.com>
2020-08-28 14:58:02 +0300

    fix naming

--- src/features/channels/ChannelsHeader.jsx
+++ src/features/channels/ChannelsHeader.jsx
@@ -3,16 +3,16 @@ import Button from 'react-bootstrap/Button';
 import Add from './modals/Add.jsx';
 
 const ChannelsHeader = () => {
-  const [addFormShow, setAddFormShow] = useState(false);
+  const [showAddingForm, setShowAddingForm] = useState(false);
 
-  const openAddForm = () => setAddFormShow(true);
-  const closeAddForm = () => setAddFormShow(false);
+  const openAddingForm = () => setShowAddingForm(true);
+  const closeAddingForm = () => setShowAddingForm(false);
 
   return (
     <div className="d-flex justify-content-between p-1 mb-2">
       <h4>Channels</h4>
-      <Button variant="outline-secondary" onClick={openAddForm}><strong>+</strong></Button>
-      {addFormShow && <Add onHide={closeAddForm} />}
+      <Button variant="outline-secondary" onClick={openAddingForm}><strong>+</strong></Button>
+      {showAddingForm && <Add onHide={closeAddingForm} />}
     </div>
   );
 };
