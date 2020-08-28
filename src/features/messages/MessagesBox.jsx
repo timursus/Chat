cd9ca8e8d94a54572208d4fa609095cc5850a0a1
Timursus <redundant09@gmail.com>
2020-08-28 15:11:10 +0300

    refactor messages selection

--- src/features/messages/MessagesBox.jsx
+++ src/features/messages/MessagesBox.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const MessagesBox = ({ currentChannelId }) => {
-  const { messages } = useSelector(
-    (state) => state.messagesInfo,
+  const currentMessages = useSelector(
+    (state) => state.messagesInfo.messages.filter((m) => m.channelId === currentChannelId),
   );
   return (
     <div className="border border-primary rounded overflow-auto text-break h-100 mb-2 p-2">
-      {messages
-        .filter(({ channelId }) => channelId === currentChannelId)
-        .map(({ message, id, username }) => (
-          <div key={id}>
-            <strong className="text-monospace">{`${username}: `}</strong>
-            <span>{message}</span>
-          </div>
-        ))}
+      {currentMessages.map(({ message, id, username }) => (
+        <div key={id}>
+          <strong className="text-monospace">{`${username}: `}</strong>
+          <span>{message}</span>
+        </div>
+      ))}
     </div>
   );
 };
