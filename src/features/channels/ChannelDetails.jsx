40ac2dcc0253d88ea608bba43e9f870357160770
Timursus <redundant09@gmail.com>
2020-08-29 22:57:33 +0300

    refactor naming #2

--- src/features/channels/ChannelDetails.jsx
+++ src/features/channels/ChannelDetails.jsx
@@ -4,10 +4,10 @@ import Remove from './modals/Remove.jsx';
 import Rename from './modals/Rename.jsx';
 
 const ChannelDetails = ({ currentChannel }) => {
-  const [showForm, setShowForm] = useState(null);
-  const openRemovingForm = () => setShowForm('removing');
-  const openRenamingForm = () => setShowForm('renaming');
-  const closeForm = () => setShowForm(null);
+  const [showingForm, setShowingForm] = useState(null);
+  const showRemovingForm = () => setShowingForm('removing');
+  const showRenamingForm = () => setShowingForm('renaming');
+  const hideForm = () => setShowingForm(null);
 
   const getEditButtons = () => {
     if (!currentChannel.removable) {
@@ -15,8 +15,8 @@ const ChannelDetails = ({ currentChannel }) => {
     }
     return (
       <div className="flex-shrink-0">
-        <Button variant="outline-secondary" size="sm" onClick={openRenamingForm}>Rename</Button>
-        <Button variant="outline-danger" size="sm" className="ml-1" onClick={openRemovingForm}>Delete</Button>
+        <Button variant="outline-secondary" size="sm" onClick={showRenamingForm}>Rename</Button>
+        <Button variant="outline-danger" size="sm" className="ml-1" onClick={showRemovingForm}>Delete</Button>
       </div>
     );
   };
@@ -25,8 +25,8 @@ const ChannelDetails = ({ currentChannel }) => {
     <div className="d-flex justify-content-between p-2">
       <h5 className="text-truncate text-info mb-1 pb-1">{currentChannel.name}</h5>
       {getEditButtons()}
-      {showForm === 'removing' && <Remove channel={currentChannel} onHide={closeForm} />}
-      {showForm === 'renaming' && <Rename channel={currentChannel} onHide={closeForm} />}
+      {showingForm === 'removing' && <Remove channel={currentChannel} onHide={hideForm} />}
+      {showingForm === 'renaming' && <Rename channel={currentChannel} onHide={hideForm} />}
     </div>
   );
 };
--- src/features/channels/ChannelsHeader.jsx
+++ src/features/channels/ChannelsHeader.jsx
@@ -3,16 +3,16 @@ import Button from 'react-bootstrap/Button';
 import Add from './modals/Add.jsx';
 
 const ChannelsHeader = () => {
-  const [showAddingForm, setShowAddingForm] = useState(false);
+  const [isShowingAddForm, toggleShowingAddForm] = useState(false);
 
-  const openAddingForm = () => setShowAddingForm(true);
-  const closeAddingForm = () => setShowAddingForm(false);
+  const showAddForm = () => toggleShowingAddForm(true);
+  const hideAddForm = () => toggleShowingAddForm(false);
 
   return (
     <div className="d-flex justify-content-between p-1 mb-2">
       <h4>Channels</h4>
-      <Button variant="outline-secondary" onClick={openAddingForm}><strong>+</strong></Button>
-      {showAddingForm && <Add onHide={closeAddingForm} />}
+      <Button variant="outline-secondary" onClick={showAddForm}><strong>+</strong></Button>
+      {isShowingAddForm && <Add onHide={hideAddForm} />}
     </div>
   );
 };
