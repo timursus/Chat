91dc1f11275f65600704425a059fa601ba275f9e
Timursus <redundant09@gmail.com>
2021-01-07 20:08:58 +0300

    rename bin

--- server/bin/timChat.js
+++ server/bin/timChat.js
@@ -3,7 +3,9 @@
 import getApp from '..';
 
 const port = process.env.PORT || 5000;
+const address = '0.0.0.0';
 const app = getApp({ port });
-app.listen(port, '0.0.0.0', () => {
+
+app.listen(port, address, () => {
   console.log(`Server has been started on ${port}`);
 });
